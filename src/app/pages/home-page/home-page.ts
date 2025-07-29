99cd459db6c15d9100a7b553a6f37eb660c19b55
Nicolas_Coelho_Mansan <nicolas.mansan@outlook.com>
2025-07-29 01:11:08 -0300

    Seção de contato

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -4,10 +4,11 @@ import { Brands } from "../../components/brands/brands";
 import { Reviews } from "../../components/reviews/reviews";
 import { SolutionsCarousel } from "../../components/solutions-carousel/solutions-carousel";
 import { ServicesCards } from "../../components/services-cards/services-cards";
+import { Contact } from "../../components/contact/contact";
 
 @Component({
   selector: 'app-home',
-  imports: [Banner, Brands, Reviews, SolutionsCarousel, ServicesCards],
+  imports: [Banner, Brands, Reviews, SolutionsCarousel, ServicesCards, Contact],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
