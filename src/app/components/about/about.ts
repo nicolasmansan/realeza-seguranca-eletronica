f78c9380742400e28c97a1b2ce30ee2d09d1a9b8
Nicolas_Coelho_Mansan <nicolas.mansan@outlook.com>
2025-07-30 16:08:27 -0300

    Adição de animações

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Subtitle } from "../subtitle/subtitle";
+
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-about',
@@ -7,10 +12,69 @@ import { Subtitle } from "../subtitle/subtitle";
   templateUrl: './about.html',
   styleUrl: './about.scss'
 })
-export class About {
+export class About implements AfterViewInit {
   public yearsExpirience: number = 20;
 
-  constructor() {
+  @ViewChild('expirienceNumber') expirienceNumber!: ElementRef<HTMLDivElement>;
+  @ViewChild('clientNumber') clientNumber!: ElementRef<HTMLDivElement>;
+  @ViewChild('gatesNumber') gatesNumber!: ElementRef<HTMLDivElement>;
+  @ViewChild('camerasNumber') camerasNumber!: ElementRef<HTMLDivElement>;
+
+  constructor(private el: ElementRef) {
     this.yearsExpirience = new Date().getFullYear() - new Date('1977-06-01').getFullYear();
+  }
+
+  ngAfterViewInit(): void {
+    if (typeof window === 'undefined') return;
+    requestAnimationFrame(() => {
+      gsap.to(this.expirienceNumber.nativeElement, {
+        innerText: this.yearsExpirience,
+        duration: 2,
+        ease: 'power1.out',
+        snap: { innerText: 1 },
+        scrollTrigger: {
+          trigger: this.expirienceNumber.nativeElement,
+          start: 'top 80%',
+          toggleActions: 'play none none none'
+        }
+      });
+
+      gsap.to(this.clientNumber.nativeElement, {
+        innerText: 1000,
+        duration: 2.5,
+        ease: 'power1.out',
+        snap: { innerText: 1 },
+        scrollTrigger: {
+          trigger: this.clientNumber.nativeElement,
+          start: 'top 80%',
+          toggleActions: 'play none none none'
+        }
+      });
+
+      gsap.to(this.gatesNumber.nativeElement, {
+        innerText: 5000,
+        duration: 3,
+        ease: 'power1.out',
+        snap: { innerText: 1 },
+        scrollTrigger: {
+          trigger: this.gatesNumber.nativeElement,
+          start: 'top 80%',
+          end: 'top 40%',
+          toggleActions: 'play none none none'
+        }
+      });
+
+      gsap.to(this.camerasNumber.nativeElement, {
+        innerText: 10000,
+        duration: 3.5,
+        ease: 'power1.out',
+        snap: { innerText: 1 },
+        scrollTrigger: {
+          trigger: this.camerasNumber.nativeElement,
+          start: 'top 80%',
+          toggleActions: 'play none none none'
+        }
+      });
+    });
   }
 }
--- src/app/components/services-cards/services-cards.ts
+++ src/app/components/services-cards/services-cards.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-import { Service, servicesData } from '../../repositories/services';
-import { Subtitle } from "../subtitle/subtitle";
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import { Subtitle } from '../subtitle/subtitle';
+import { servicesData } from '../../repositories/services';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-services-cards',
@@ -8,10 +12,48 @@ import { Subtitle } from "../subtitle/subtitle";
   templateUrl: './services-cards.html',
   styleUrl: './services-cards.scss'
 })
-export class ServicesCards {
-  public services: Service[];
+export class ServicesCards implements AfterViewInit {
+  public services = servicesData;
 
-  constructor () {
-    this.services = servicesData;
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    const section = this.el.nativeElement as HTMLElement;
+
+    gsap.from(section.querySelector('.from-left'), {
+      x: -200,
+      opacity: 0,
+      duration: 1,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: section.querySelector('.from-left'),
+        start: 'top 80%',
+        end: 'top 40%',
+        toggleActions: 'play reverse play reverse',
+        scrub: true
+      }
+    });
+
+    gsap.from(section.querySelector('.from-bottom'), {
+      y: 200,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: section.querySelector('.from-bottom'),
+        start: 'top 80%',
+        end: 'top 30%',
+        scrub: true
+      }
+    });
+
+    gsap.from(section.querySelector('.from-right'), {
+      x: 200,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: section.querySelector('.from-right'),
+        start: 'top 80%',
+        end: 'top 40%',
+        scrub: true
+      }
+    });
   }
 }
