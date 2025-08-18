60ad24fa16076119baf1a2f8474884398e5ac4fb
Nicolas_Coelho_Mansan <nicolas.mansan@outlook.com>
2025-08-18 20:38:39 -0300

    Página de sindivos v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePage } from './pages/home-page/home-page';
 import { IronPage } from './pages/iron-page/iron-page';
 import { SecurityPage } from './pages/security-page/security-page';
 import { GatePage } from './pages/gate-page/gate-page';
+import { SindicoPage } from './pages/sindico-page/sindico-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'serralheria', component: IronPage },
   { path: 'seguranca-eletronica', component: SecurityPage },
   { path: 'portoes-automaticos', component: GatePage },
+  { path: 'sindicos', component: SindicoPage },
 ];
